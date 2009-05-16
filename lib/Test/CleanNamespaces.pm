632da200e23c6b42c7e1263369ba36093a08869d
Florian Ragwitz <rafl@debian.org>
2009-05-16 18:28:42 +0200

    Add docs.

--- lib/Test/CleanNamespaces.pm
+++ lib/Test/CleanNamespaces.pm
@@ -28,10 +28,50 @@ BEGIN {
     *import = subname __PACKAGE__ . '::import', \&import;
 }
 
-{
-    my $Test = Test::Builder->new;
-    sub builder { $Test }
-}
+=head1 SYNOPSIS
+
+    use strict;
+    use warnings;
+    use Test::CleanNamespaces;
+
+    all_namespaces_clean;
+
+=head1 DESCRIPTION
+
+This module lets you check your module's namespaces for imported functions you
+might have forgotten to remove with L<namespace::autoclean> or
+L<namespace::clean> and are therefor available to be called as methods, which
+usually isn't want you want.
+
+=head1 FUNCTIONS
+
+All functions are exported by default.
+
+=head2 namespaces_clean
+
+    namespaces_clean('YourModule', 'AnotherModule');
+
+Tests every specified namespace for uncleaned imports. If the module couldn't
+be loaded it will be skipped.
+
+=head2 all_namespaces_clean
+
+    all_namespaces_clean;
+
+Runs C<namespaces_clean> for all modules in your distribution.
+
+=head1 METHODS
+
+The exported functions are constructed using the the following methods. This is
+what you want to override if you're subclassing this module..
+
+=head2 build_namespaces_clean
+
+    my $coderef = Test::CleanNamespaces->build_namespaces_clean;
+
+Returns a coderef that will be exported as C<namespaces_clean>.
+
+=cut
 
 sub build_namespaces_clean {
     my ($class, $name) = @_;
@@ -58,6 +98,15 @@ sub build_namespaces_clean {
     };
 }
 
+=head2 build_all_namespaces_clean
+
+    my $coderef = Test::CleanNamespaces->build_namespaces_clean;
+
+Returns a coderef that will be exported as C<all_namespaces_clean>. It will use
+the C<find_modules> method to get the list of modules to check.
+
+=cut
+
 sub build_all_namespaces_clean {
     my ($class, $name) = @_;
     my $namespaces_clean = $class->build_namespaces_clean($name);
@@ -67,6 +116,15 @@ sub build_all_namespaces_clean {
         $namespaces_clean->(@modules);
     };
 }
+
+=head2 find_modules
+
+    my @modules = Test::CleanNamespaces->find_modules;
+
+Returns a list of modules in the current distribution. It'll search in
+C<blib/>, if it exists. C<lib/> will be searched otherwise.
+
+=cut
 
 sub find_modules {
     my ($class) = @_;
@@ -78,6 +136,19 @@ sub find_modules {
         join '::' => splitdir($_);
     } File::Find::Rule->perl_module->in(-e 'blib' ? 'blib' : 'lib');
     return @modules;
+}
+
+=head2 builder
+
+    my $builder = Test::CleanNamespaces->builder;
+
+Returns the C<Test::Builder> used by the test functions.
+
+=cut
+
+{
+    my $Test = Test::Builder->new;
+    sub builder { $Test }
 }
 
 1;
